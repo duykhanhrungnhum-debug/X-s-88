00490f7ef33de11fc277e5bd4c3e31cd3a3c13bf
duykhanhrungnhum-debug <duykhanhrungnhum@gmail.com>
2026-09-17 15:38:07 +0700

    test: cover region URLs and source date protection

--- tests/test_minhngoc_html.py
+++ tests/test_minhngoc_html.py
@@ -2,10 +2,16 @@ from datetime import date
 
 import pytest
 
-from scraper.sources.minhngoc_html import HostRateLimiter, build_region_url, parse_html
+from scraper.sources.minhngoc_html import (
+    HostRateLimiter,
+    build_region_url,
+    extract_region_result_date,
+    parse_html,
+)
 
 
 MB_FIXTURE = """
+<div>KẾT QUẢ XỔ SỐ Miền Bắc - 16/09/2026</div>
 <table class="bkqmienbac"><tr><td><table class="bkqtinhmienbac">
 <tr><td class="giai1l">Giải nhất</td><td class="giai1"><div>12345</div></td></tr>
 <tr><td class="giai2l">Giải nhì</td><td class="giai2"><div>01234</div><div>56789</div></td></tr>
@@ -20,6 +26,7 @@ MB_FIXTURE = """
 """
 
 MN_FIXTURE = """
+<div>KẾT QUẢ XỔ SỐ Miền Nam - 16/09/2026</div>
 <table class="bkqmiennam"><tr><td><table class="rightcl">
 <td class="tinh">Tây Ninh</td>
 <td class="giai8"><div>09</div></td>
@@ -28,11 +35,34 @@ MN_FIXTURE = """
 </table></td></tr></table>
 """
 
+MT_FIXTURE = """
+<div>KẾT QUẢ XỔ SỐ Miền Trung - 16/09/2026</div>
+<section><table class="some-regional-wrapper"><tr><td><table class="rightcl">
+<td class="tinh">Đà Nẵng</td>
+<td class="giai8"><div>23</div></td>
+<td class="giai1"><div>90069</div></td>
+<td class="giaidb"><div>173506</div></td>
+</table></td></tr></table></section>
+"""
 
-def test_build_region_url_uses_verified_daily_page():
-    assert build_region_url("mien-nam", date(2026, 9, 15)) == (
-        "https://www.minhngoc.net.vn/ket-qua-xo-so/15-09-2026.html"
+
+def test_build_region_url_uses_region_specific_daily_pages():
+    assert build_region_url("mien-bac", date(2026, 9, 15)) == (
+        "https://www.minhngoc.net.vn/ket-qua-xo-so/mien-bac/15-09-2026.html"
     )
+    assert build_region_url("mien-trung", date(2026, 9, 15)) == (
+        "https://www.minhngoc.net.vn/ket-qua-xo-so/mien-trung/15-09-2026.html"
+    )
+    assert build_region_url("mien-nam", date(2026, 9, 15)) == (
+        "https://www.minhngoc.net.vn/ket-qua-xo-so/mien-nam/15-09-2026.html"
+    )
+
+
+def test_extract_region_result_date():
+    assert extract_region_result_date(MB_FIXTURE, "mien-bac") == date(2026, 9, 16)
+    assert extract_region_result_date(MT_FIXTURE, "mien-trung") == date(2026, 9, 16)
+    assert extract_region_result_date(MN_FIXTURE, "mien-nam") == date(2026, 9, 16)
+    assert extract_region_result_date("<html>no results</html>", "mien-nam") is None
 
 
 def test_parse_mien_bac_preserves_leading_zeroes():
@@ -56,6 +86,12 @@ def test_parse_mien_nam_extracts_province_and_numbers():
     assert any(row.prize == "Giải Đặc Biệt" and row.numbers == ("012345",) for row in rows)
 
 
+def test_parse_mien_trung_uses_province_tables():
+    rows = parse_html(MT_FIXTURE, "mien-trung")
+    assert any(row.province == "Đà Nẵng" and row.prize == "Giải tám" and row.numbers == ("23",) for row in rows)
+    assert any(row.province == "Đà Nẵng" and row.prize == "Giải nhất" and row.numbers == ("90069",) for row in rows)
+
+
 def test_rate_limiter_enforces_five_seconds_per_host():
     clock = [100.0]
     sleeps: list[float] = []
@@ -68,7 +104,7 @@ def test_rate_limiter_enforces_five_seconds_per_host():
         clock[0] += seconds
 
     limiter = HostRateLimiter(interval_seconds=5.0, monotonic=monotonic, sleeper=sleeper)
-    url = "https://www.minhngoc.net.vn/ket-qua-xo-so/15-09-2026.html"
+    url = "https://www.minhngoc.net.vn/ket-qua-xo-so/mien-nam/15-09-2026.html"
     limiter.wait(url)
     clock[0] += 1.0
     limiter.wait(url)
